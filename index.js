1e5e8c7f260b49baf06f11f9abe4373c388d08aa
Gimena <gimenabinaghi@hotmail.com>
2021-01-23 14:56:50 -0300

    responsive

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var cad= `
                           <a class="nav-link" href="education.html" onclick="educationRedirect()"> Education </a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="skill.html" onclick="skillRedirect()"> Skill </a>
+                          <a class="nav-link" href="skill.html" onclick="skillRedirect()"> Skills </a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link" href="job.html" onclick="jobRedirect()"> Experience </a>
@@ -31,11 +31,11 @@ document.getElementById("header").innerHTML=cad;
 
 var cad= `
 <div class="social">
-                    <a href="https://www.linkedin.com/in/gimenabinaghi/" class="icon-linkedin-square" target="_blank"><p> gimena-binaghi </p></a>
-                    <a href="https://github.com/gibinaghi?tab=repositories" class="icon-github" target="_blank"><p> gibinaghi </p></a>
+                    <a href="https://www.linkedin.com/in/gimenabinaghi/" class="icon-linkedin-square" target="_blank"><p></p></a>
+                    <a href="https://github.com/gibinaghi?tab=repositories" class="icon-github" target="_blank"><p></p></a>
                 </div>
                 <div> 
-                    <p class="copy">  &copy; 2020, All rights reserved - | María Gimena Binaghi |</p>
+                    <p class="copy">  &copy; 2020, All rights reserved <br> | María Gimena Binaghi |</p>
                 </div>
                   
 `
